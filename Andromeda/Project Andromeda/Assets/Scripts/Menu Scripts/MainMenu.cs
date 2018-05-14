3978414ea68b58abfb5ee1714efd76f16b082599
TysonMatthews <35093010+TysonMatthews@users.noreply.github.com>
2018-05-14 12:38:36 -0500

    5/14/18 - T

---
+++ Andromeda/Project Andromeda/Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject Menu;
+	public GameObject optionsMenu;
+	public GameObject bgCover;
+	public RawImage dummyImg;
+	public GameObject savedGames;
+	public GameObject bgCoverCloseBttn;
+
+	void Start () {
+		dummyImg.enabled = false;
+		bgCover.SetActive(false);
+		bgCoverCloseBttn.SetActive(false);
+	}
+	
+	void Update () {
+		
+	}
+
+	public void ContinueBttn () {
+		
+	}
+
+	public void NGBttn () {
+		
+	}
+
+	public void LGBttn () {
+		bgCover.SetActive(true);
+		bgCoverCloseBttn.SetActive(true);
+		savedGames.SetActive(true);
+	}
+
+	public void OptionsBttn () {
+		Menu.SetActive(false);
+		bgCover.SetActive(false);
+		optionsMenu.SetActive(true);
+	}
+
+	public void ExitBttn () {
+		Application.Quit();
+	}
+
+	public void BGCoverCloseBttn () {
+		bgCover.SetActive(false);
+		bgCoverCloseBttn.SetActive(false);
+		savedGames.SetActive(false);
+	}
+
+	
+
+
+}
---
+++ Andromeda/Project Andromeda/Assets/Scripts/Menu Scripts/OptionsMenu.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+
+	public GameObject Menu;
+	public GameObject optionsMenu;
+	public GameObject bgCover;
+	public GameObject videoOptions;
+	public GameObject audioOptions;
+	public GameObject controlOptions;
+	public GameObject credits;
+	public GameObject bgCoverCloseBttn;
+
+	void Start () {
+		bgCover.SetActive(false);
+		videoOptions.SetActive(false);
+		audioOptions.SetActive(false);
+		controlOptions.SetActive(false);
+		credits.SetActive(false);
+		bgCoverCloseBttn.SetActive(false);
+	}
+	
+	void Update () {
+		//Credits Exit
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			credits.SetActive(false);
+			bgCover.SetActive(false);
+		}
+	}
+
+	public void VideoBttn () {
+		bgCover.SetActive(true);
+		videoOptions.SetActive(true);
+		bgCoverCloseBttn.SetActive(true);
+		audioOptions.SetActive(false);
+		controlOptions.SetActive(false);
+		credits.SetActive(false);
+	}
+
+	public void AudioBttn () {
+		bgCover.SetActive(true);
+		audioOptions.SetActive(true);
+		bgCoverCloseBttn.SetActive(true);
+		videoOptions.SetActive(false);
+		controlOptions.SetActive(false);
+		credits.SetActive(false);
+	}
+
+	public void ControlsBttn () {
+		bgCover.SetActive(true);
+		controlOptions.SetActive(true);
+		bgCoverCloseBttn.SetActive(true);
+		videoOptions.SetActive(false);
+		audioOptions.SetActive(false);
+		credits.SetActive(false);
+	}
+
+	public void CreditsBttn () {
+		bgCover.SetActive(true);
+		credits.SetActive(true);
+		videoOptions.SetActive(false);
+		audioOptions.SetActive(false);
+		controlOptions.SetActive(false);
+		bgCoverCloseBttn.SetActive(false);
+	}
+
+	public void BackBttn () {
+		videoOptions.SetActive(false);
+		audioOptions.SetActive(false);
+		controlOptions.SetActive(false);
+		credits.SetActive(false);
+		optionsMenu.SetActive(false);
+		bgCover.SetActive(false);
+		bgCoverCloseBttn.SetActive(false);
+		Menu.SetActive(true);
+		
+	}
+
+	public void BGCoverCloseBttn () {
+		bgCover.SetActive(false);
+		bgCoverCloseBttn.SetActive(false);
+		videoOptions.SetActive(false);
+		audioOptions.SetActive(false);
+		controlOptions.SetActive(false);
+		credits.SetActive(false);
+	}
+}
